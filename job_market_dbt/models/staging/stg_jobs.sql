2d32ae03299ea7345e165e4a4104693a80b2d9d5
divyakputhussery <divyasharath86@icloud.com>
2026-04-15 17:00:14 +1000

    feat: add dbt staging model for jobs

---
+++ job_market_dbt/models/staging/stg_jobs.sql
@@ -0,0 +1,13 @@
+SELECT
+    job_id,
+    title,
+    company,
+    location,
+    work_type,
+    category,
+    posted_date,
+    latitude,
+    longitude,
+    salary_is_predicted,
+    collected_at
+FROM public.jobs
